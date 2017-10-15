df62aa1fce4c0def9956f62c137bfd8dd93546da
Simon Emms <simon@simonemms.com>
2017-10-15 17:22:49 +0100

    Removed the repo update interval from the main file

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -15,7 +15,7 @@ import store from './store';
 
 Vue.use(Vuetify);
 
-const app = new Vue({
+new Vue({
   components: {
     App,
   },
@@ -24,33 +24,3 @@ const app = new Vue({
   store,
   template: '<App />',
 }).$mount('#app');
-
-let gettingStatus = false;
-
-function getRepoStatus () {
-  /* Check if we're currently getting the status - don't duplicate */
-  if (gettingStatus) {
-    return Promise.resolve(app.$store.getters.repos);
-  }
-
-  gettingStatus = true;
-
-  return app.$store.dispatch('loadLatestStatus')
-    .then(() => {
-      gettingStatus = false;
-
-      return app.$store.getters.repos;
-    })
-    .catch((err) => {
-      gettingStatus = false;
-
-      return Promise.reject(err);
-    });
-}
-
-/* Run a background task to get the repo statuses */
-getRepoStatus();
-
-setInterval(() => {
-  getRepoStatus();
-}, 10000);
