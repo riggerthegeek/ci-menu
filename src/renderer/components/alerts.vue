0b29c4874c21a7dec80512070aa42ec1d6beaa69
Simon Emms <simon@simonemms.com>
2017-10-30 21:00:27 +0000

    Notifications basically working

--- src/renderer/components/alerts.vue
+++ src/renderer/components/alerts.vue
@@ -1,5 +1,59 @@
 <template lang="jade">
-  div alerts page
+  div
+    v-alert(
+      color="success",
+      icon="check_circle",
+      value="true",
+      transition="scale-transition"
+      v-if="saving"
+    ) {{ $t('common:SAVED') }}
+
+    v-container(
+      fluid
+      grid-list-md
+    )
+      v-layout(
+        row
+        wrap
+      )
+        v-flex
+
+          v-form(
+            ref="form"
+            lazy-validation
+          )
+
+            v-select(
+              :items="alertOpts",
+              :label="$t('notifications:WHEN_TO_ALERT')",
+              v-model="data.notify",
+              prepend-icon="warning"
+            )
+
+            v-switch(
+              :label="$t('notifications:SHOW_DND')"
+              v-model="data.dnd",
+              v-if="data.notify !== 'never'"
+            )
+
+            div(
+              v-if="data.dnd && data.notify !== 'never'",
+            )
+
+              v-select(
+                :label="$t('notifications:DND_START')"
+                v-model="data.dndStart",
+                :items="times",
+                prepend-icon="access_time"
+              )
+
+              v-select(
+                :label="$t('notifications:DND_END')"
+                v-model="data.dndEnd",
+                :items="times",
+                prepend-icon="access_time"
+              )
+
 </template>
 
 <script>
@@ -14,9 +68,76 @@
   /* Files */
 
   export default {
+
     data () {
-      console.log('alerts');
-      return {};
+      const state = {
+        id: 'id',
+        status: 'success',
+        title: 'hello/world',
+      };
+      const previousState = {
+        status: 'fail',
+      };
+
+      this.$store.dispatch('notify', {
+        i18n: this.$i18n,
+        newState: state,
+        oldState: previousState,
+      });
+
+
+      const times = [];
+
+      for (let hour = 0; hour < 24; hour += 1) {
+        for (let mins = 0; mins < 60; mins += 30) {
+          const time = {
+            hour,
+            mins,
+          };
+
+          if (time.hour < 10) {
+            time.hour = `0${time.hour}`;
+          }
+
+          if (time.mins < 10) {
+            time.mins = `0${time.mins}`;
+          }
+
+          times.push(`${time.hour}:${time.mins}`);
+        }
+      }
+
+      return {
+        alertOpts: this.$store.getters.notifyOpts.map(value => ({
+          text: this.$i18n.t(`notifications:OPT_${value.toUpperCase()}`),
+          value,
+        })),
+        data: this.$store.getters.notify,
+        saving: false,
+        times,
+      };
     },
+
+    methods: {
+
+      save () {
+        this.saving = true;
+
+        setTimeout(() => {
+          this.saving = false;
+        }, 5000);
+
+        return this.$store.dispatch('saveNotification', this.data);
+      },
+
+    },
+
+    watch: {
+      'data.dnd': 'save',
+      'data.dndEnd': 'save',
+      'data.dndStart': 'save',
+      'data.notify': 'save',
+    },
+
   };
 </script>
--- src/renderer/store/notifications.js
+++ src/renderer/store/notifications.js
@@ -5,15 +5,31 @@
 /* Node modules */
 
 /* Third-party modules */
+import { _ } from 'lodash';
+import { remote } from 'electron';
+import Vue from 'vue/dist/vue.min';
 
 /* Files */
+
+const logger = remote.app.logger;
+
+const storageKey = 'ci-menu-notifications';
+
+let stateData = {};
+
+try {
+  stateData = JSON.parse(localStorage[storageKey]);
+} catch (err) {
+  /* Use default */
+}
 
 export default {
 
   actions: {
 
-    notify ({ state }, { i18n, newState, oldState }) {
-      let toNotify = false;
+    notify ({ getters, state }, { i18n, newState, oldState }) {
+      /* Default to always notify */
+      let toNotify = true;
 
       const buildLabel = newState.id;
       const newStatus = newState.status;
@@ -30,8 +46,8 @@ export default {
           break;
 
         default:
-          /* Always notify */
-          toNotify = true;
+          /* Never notify */
+          toNotify = false;
           break;
       }
 
@@ -40,7 +56,7 @@ export default {
         name: newState.title,
       });
 
-      if (toNotify) {
+      if (toNotify && !getters.doNotDisturb()) {
         return new Notification(msg, {
           tag: build,
         });
@@ -49,22 +65,95 @@ export default {
       return false;
     },
 
+    saveNotification ({ commit }, { dnd, dndEnd, dndStart, notify }) {
+      const data = {
+        dnd,
+        dndEnd,
+        dndStart,
+        notify,
+      };
+
+      /* Save to local storage */
+      localStorage[storageKey] = JSON.stringify(data);
+
+      /* Update the state */
+      commit('updateNotification', data);
+
+      return data;
+    },
+
   },
 
   getters: {
 
-    notify: state => state.notify,
+    /**
+     * Do Not Disturb
+     *
+     * Are we in a do not disturb
+     * period?
+     *
+     * @param {*} state
+     * @returns {boolean}
+     */
+    doNotDisturb: state => () => {
+      if (state.dnd) {
+        /*
+        const dndStart = state.dndStart.split(':');
+        const dndEnd = state.dndEnd.split(':');
+
+        const dates = [];
+
+        const startTime = moment()
+          .hour(dndStart[0])
+          .minute(dndStart[1])
+          .seconds(0)
+          .milliseconds(0);
+
+        const endTime = moment()
+          .hour(dndEnd[0])
+          .minute(dndEnd[1])
+          .seconds(0)
+          .milliseconds(0);
+          */
+
+        return true;
+      }
+
+      /* Nope */
+      return false;
+    },
+
+    notify: state => ({
+      dnd: state.dnd,
+      dndEnd: state.dndEnd,
+      dndStart: state.dndStart,
+      notify: state.notify,
+    }),
 
     notifyOpts: () => [
       'all', // Notify whenever a build has finished
       'change', // Notify when a change is detected
       'fail', // Notify when new status is not 'Success'
+      'never',
     ],
 
   },
 
-  state: {
-    notify: 'change',
+  mutations: {
+
+    updateNotification (state, data) {
+      logger.trigger('trace', 'Updating repo state');
+
+      Vue.set(state, 'repos', data);
+    },
+
   },
+
+  state: _.defaults(stateData, {
+    dnd: false,
+    dndEnd: '08:00',
+    dndStart: '22:00',
+    notify: 'change',
+  }),
 
 };
