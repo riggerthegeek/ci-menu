ecdc794be2c3e2031875788978ebdbea60bbce9a
Simon Emms <simon@simonemms.com>
2017-11-02 20:12:24 +0000

    Fixed the logger error

--- src/renderer/components/layout.vue
+++ src/renderer/components/layout.vue
@@ -235,7 +235,7 @@
 
                   if (states.history.building && !states.latest.building) {
                     /* We've just finished building */
-                    logger.trigger('info', 'Building has finished', {
+                    logger('info', 'Building has finished', {
                       repo,
                       history,
                     });
